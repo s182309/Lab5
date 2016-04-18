995d054785f93e8254989b2bb5554b901da12e4a
s182309 <s182309@studenti.polito.it>
2016-04-18 20:29:52 +0200

    branch inlist incompleto

--- Lab5_Ruzzle/src/it/polito/tdp/dao/ParolaDAO.java
+++ Lab5_Ruzzle/src/it/polito/tdp/dao/ParolaDAO.java
@@ -5,37 +5,54 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ParolaDAO {
+	
+	Set <String> dictionary;
 	
 	private String jdbcURL;
 	
 	public ParolaDAO () {
 		jdbcURL = "jdbc:mysql://localhost/dizionario?user=root&password=root";
+		dictionary = new HashSet <String>() ;
 	}
 	
-	public boolean isPresent(String s){
+	public void load(){
 		
 		try {
 			Connection conn = DriverManager.getConnection(jdbcURL);
-			String sql = "select nome from parola where nome = ? ";
+			String sql = "select nome from parola";
 			PreparedStatement st = conn.prepareStatement(sql);
-			st.setString(1, s);
 			ResultSet res = st.executeQuery();
-			if(res.next()){
-				return true;
+			while(res.next()){
+				dictionary.add(res.getString("nome"));
 			}
-			return false;
+			res.close();
+			conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	
+	public boolean isPresent(String s){
+		return dictionary.contains(s);
+	}
+	
+	public boolean isContenuto(String s){
+		for(String str : dictionary)
+			if(str.contains(s))
+				return true;
 		return false;
 	}
 	
 	public static void main (String[] args){
 		ParolaDAO p = new ParolaDAO();
-		System.out.println(p.isPresent("ciao"));
+		p.load();
+		System.out.println(p.isContenuto("ciao"));
 	}
 
 }
